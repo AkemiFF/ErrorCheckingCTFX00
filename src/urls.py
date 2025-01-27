32d34712d31fdfd593627c4e13f9a3f00360442d
Brad Akemi <mirado.akemi@gmail.com>
2025-01-27 20:21:08 +0300

    url test

--- src/urls.py
+++ src/urls.py
@@ -18,9 +18,15 @@ from django.contrib import admin
 from django.urls import path
 
 from accounts.views import MyTokenObtainPairView, MyTokenRefreshView
+from core.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('test/', test, name='test'),
+    path('stop_test/', stop_test, name='stop_test'),
+    path('<int:port>/<str:path>', proxy_to_docker, name='proxy_to_docker'),
+    # path('challenge/<int:challenge_id>/start/', views.start_challenge, name='start_challenge'),
+
     path('token/refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
 ]
